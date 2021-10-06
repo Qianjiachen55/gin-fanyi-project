9fda707fc1c90ca2936d0b4ff0ee3d0373787642
jaronnie <nstar@gocloudcoder.com>
2021-10-06 04:12:27 +0000

    feat(test): test global logger

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,13 +1,13 @@
 package controllers
 
 import (
-	"fmt"
+	"github.com/Qianjiachen55/gin-fanyi-project/global"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 func Test(ctx *gin.Context)  {
 	ip := viper.GetString("mysql.ip")
-	fmt.Println(ip)
+	global.GFP_LOGGER.Debug(ip)
 	ctx.String(200,"hello world!!!!")
 }
