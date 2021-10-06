0ec7c3407517a13d309efe262ed61f749126c701
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-10-06 17:38:35 +0800

    docs(test): testtt_commit

--- .github/scripts/checkLatestCommit.sh
+++ .github/scripts/checkLatestCommit.sh
@@ -4,6 +4,20 @@
 
 # shellcheck disable=SC2034
 commit=$1
+echo $commit
+# shellcheck disable=SC1036
+# shellcheck disable=SC1036
+# shellcheck disable=SC1073
+# shellcheck disable=SC1072
+#msg_re="^(feat|fix)\([A-Za-z0-9_]*\)\:\s[A-Za-z0-9_]*"
+msg_re="^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?: .{1,100}"
+if [[ $commit =~ $msg_re ]]
+then
+  echo "commit success"
+else
+  exit 1
+fi
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # READEME
 
+---------
+
 nothing here!
+
+#############
