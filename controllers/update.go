881624c37fbb34e06f569a5ba07d8d8b34774796
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-10-13 00:25:09 +0800

    fix(conf): fix conf & add goroutine

--- controllers/update.go
+++ controllers/update.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/Qianjiachen55/gin-fanyi-project/global"
 	"github.com/Qianjiachen55/gin-fanyi-project/service"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cast"
@@ -9,12 +10,12 @@ import (
 )
 
 func GetUpdate(ctx *gin.Context)  {
-	id := ctx.Param("id")
+	id := ctx.Query("id")
+	global.GFP_LOGGER.Debug(id)
 	go func() {
 		for count:= 0;count<100;count++	{
 			service.UpdateCountMysql(cast.ToUint(id), cast.ToUint(count))
 			time.Sleep(time.Second)
-
 		}
 	}()
 	ctx.String(http.StatusOK,"updating......")
